632747d3deb0c4d0cdf7031bc2de913c7145f76d
Alex Erofeev <xnet141@gmail.com>
2024-10-12 02:45:48 +0300

    Save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,17 @@
 class ContactsController < ApplicationController
-  
+
+  def new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+  end
+
 end
